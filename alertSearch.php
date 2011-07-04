7fe1f906cc41bd6316187351d389b6c858a4895f
Piotr Radzikowski <radzikowski@radzikowski-Lenovo-G550.(none)>
2011-07-04 15:03:22 +0200

    create alertSearch.php for get alerts from jquery

---
+++ alertSearch.php
@@ -0,0 +1,105 @@
+<?php
+error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+
+$words = explode(' ', strtolower("Nullam eget orci sed massa venenatis commodo id congue urna. Ut risus nisi, condimentum ac suscipit ac, varius gravida risus. Suspendisse pulvinar sagittis pellentesque. Duis nec nibh a diam egestas venenatis id non lectus. Nunc purus sem, faucibus quis lobortis a, pharetra id turpis. Ut leo purus, blandit et sagittis et, consectetur vel risus. Sed quis justo orci. Donec ullamcorper arcu quis ipsum fringilla ut convallis mauris vulputate. Etiam quis urna mauris, a molestie ante. Etiam at libero risus. Aliquam non justo vel massa ultrices pretium. Sed varius sodales eros, ac sagittis tortor elementum vitae.
+Nullam blandit metus vestibulum magna luctus placerat. Nulla facilisi. Nam ipsum felis, mollis non tristique non, gravida eget nunc. Fusce risus quam, eleifend at tincidunt eleifend, convallis vel est. Nunc quis est at orci varius venenatis. Curabitur ut euismod mi. Quisque mauris erat, gravida ut laoreet eu, ullamcorper a sapien. Proin adipiscing nisl in lectus rutrum eget feugiat risus sodales. Morbi enim neque, fringilla quis posuere vel, molestie sit amet nisl. Pellentesque augue mi, auctor scelerisque convallis sit amet, aliquam eu lectus.
+Mauris sit amet sem mi. Sed rutrum sapien id tellus iaculis ac consectetur justo sodales. Proin vel nulla a tortor malesuada mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi tempus mattis risus, ut faucibus purus faucibus eu. Aenean nibh diam, consectetur et faucibus non, lacinia sit amet nulla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus dui nunc, auctor et mollis posuere, euismod adipiscing dolor. Proin ut orci diam. Proin consectetur semper metus, laoreet pellentesque enim viverra quis. Donec pharetra imperdiet quam, non malesuada tellus ullamcorper et. Maecenas tortor est, hendrerit at egestas nec, convallis sit amet nibh. Vestibulum nulla dui, suscipit ornare eleifend a, pretium eu lacus. Nulla vehicula hendrerit ultrices. Phasellus venenatis imperdiet felis ac adipiscing.
+Suspendisse potenti. Aliquam nulla nibh, posuere id scelerisque et, elementum et tortor. Maecenas rutrum augue et dui vehicula ut tincidunt dolor sodales. Quisque sit amet adipiscing dolor. Proin eget lorem orci, et tincidunt magna. Proin non diam orci, id porttitor mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris ante lacus, vestibulum sed iaculis a, auctor sit amet neque. Fusce cursus eros lectus. Cras at varius ligula. Donec tempus mauris id lectus mollis at aliquet nisi dignissim. Nulla facilisi. Suspendisse sed purus neque, sit amet tristique purus. Morbi placerat facilisis viverra. Vestibulum facilisis, mi ut luctus faucibus, augue velit venenatis velit, a tempus nulla justo vitae mi. Aenean ut purus eget libero malesuada posuere bibendum a tortor. Aenean vel odio lacus. Duis a ante nisi, at adipiscing tortor.
+Ut pharetra commodo convallis. Aenean at purus non dolor hendrerit lacinia et in augue. Aliquam commodo venenatis nisi ut ullamcorper. Nunc volutpat magna non arcu tempus aliquet. Sed ornare nisi in erat placerat venenatis. Nunc scelerisque ipsum in ligula facilisis consectetur. Sed at ante mauris. Mauris eget vestibulum turpis. Suspendisse sem ligula, cursus vitae pretium sit amet, fringilla id tortor. Fusce ut dapibus dui. Vivamus vitae nibh facilisis purus rutrum tristique in nec sem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+Cras vitae massa id neque auctor pharetra. Ut eu velit in tortor suscipit scelerisque. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse fringilla risus in lectus suscipit eget fermentum ipsum consectetur. Praesent a ultrices mauris. Morbi in mi eget eros feugiat faucibus. Nunc non dui id quam suscipit pretium. Nullam non nisl sapien, non sagittis lacus. Sed tincidunt, est eget lacinia congue, urna lectus suscipit sapien, nec sodales est mi et turpis. Mauris porttitor vestibulum odio sit amet tempor. Ut lacinia commodo sodales. Cras laoreet tempor felis vitae egestas. Phasellus volutpat arcu et nisl volutpat lobortis. In luctus tempus nulla, in vehicula mauris placerat nec. Aliquam erat volutpat. Suspendisse potenti. Sed faucibus, mi non porta sagittis, sapien risus malesuada massa, nec iaculis sapien nibh at lectus. Duis eleifend consectetur lacus sed aliquet.
+Nam nec mi quis sem congue sollicitudin et eget erat. Vivamus libero sem, sollicitudin a eleifend non, consequat eu mi. Donec faucibus pretium eros quis fermentum. Curabitur consectetur tincidunt justo, sit amet hendrerit tellus consectetur sed. Suspendisse mattis auctor justo, ac venenatis magna lacinia eu. Sed risus nulla, adipiscing quis porta sit amet, accumsan varius nulla. Vestibulum eget tristique felis. Praesent quis orci mauris, non pulvinar elit. Fusce pulvinar dolor purus. Etiam et diam purus. Suspendisse dictum viverra pretium. Pellentesque interdum, turpis sit amet posuere suscipit, ligula orci luctus diam, eu rutrum dui quam eu turpis. Suspendisse sapien odio, elementum accumsan rhoncus quis, ultricies id mauris.
+Etiam in odio non enim facilisis dictum. Integer tempor luctus vehicula. Aliquam turpis odio, ultrices vitae ultricies eget, mattis et lacus. Nunc odio nisi, ultricies ac adipiscing nec, suscipit quis massa. Etiam vel metus ipsum, ac venenatis ante. Vivamus imperdiet turpis a ligula lobortis iaculis. Etiam aliquam hendrerit mauris, vitae rhoncus libero pretium quis. Proin fringilla sapien id nisl ultricies non egestas ipsum aliquet. Curabitur eu dignissim augue. Donec dictum tempus arcu non viverra. Integer mattis diam eget ligula tempor porta. Maecenas molestie consectetur ante eget cursus.
+Fusce ac venenatis risus. In tristique lorem in elit mattis ultrices. Maecenas ligula tortor, fermentum vitae gravida ac, semper vel turpis. Nullam laoreet ante eu ante egestas sed pulvinar risus viverra. Vestibulum tristique ligula tortor. Proin pulvinar augue turpis. Maecenas tellus odio, ultricies eu consectetur nec, condimentum vitae diam. Suspendisse convallis eleifend auctor. Sed placerat nulla ac justo vulputate aliquet. Integer et neque tellus, eu tempor lectus. Vestibulum in lectus metus, quis viverra ante. Donec blandit metus in dolor vestibulum sed tristique nisi consectetur. Sed feugiat aliquam nisl. Cras quis ipsum at justo aliquam commodo eget in nibh. Curabitur lacinia euismod nisi, at cursus nisi dapibus a. Quisque id arcu risus, nec imperdiet tellus. Etiam dignissim egestas erat nec fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+Morbi sem elit, convallis non laoreet et, dignissim at est. Donec commodo placerat est, id euismod lectus fringilla eget. Proin commodo placerat erat, in varius enim ultrices vel. Duis rhoncus tincidunt nisl, eget dictum lorem interdum gravida. Nullam velit augue, auctor ac scelerisque in, blandit sagittis mauris. Vivamus id erat augue, ut blandit odio. Suspendisse potenti. Quisque dignissim commodo massa, eu tristique orci sagittis a. Vivamus turpis elit, tincidunt id placerat ut, tempus eget nisl. Fusce non neque suscipit lectus sodales malesuada eu id metus. Integer urna urna, scelerisque in adipiscing sit amet, pulvinar id urna. Suspendisse porttitor adipiscing commodo. In congue elit eget quam ornare ac rutrum odio scelerisque. Integer nec nisi lacus.
+Quisque euismod diam vitae nisl tempor varius. In molestie posuere est, vel porttitor dui dignissim in. Vestibulum at diam nec leo consequat rutrum. In odio tortor, tincidunt sed lobortis eu, posuere non orci. Suspendisse dapibus pretium ullamcorper. Sed gravida iaculis dui, eget dignissim quam dignissim sit amet. Cras porttitor auctor lectus nec egestas. Proin vitae lectus libero, sit amet varius elit. Quisque venenatis condimentum dolor, vel semper sapien pulvinar nec. Sed tempus ante id lacus cursus eu mattis lectus luctus. Quisque ullamcorper eleifend elit, vel scelerisque orci scelerisque in. Duis id lectus sit amet leo venenatis auctor eu sed ipsum. Proin pulvinar elementum odio vel interdum. Pellentesque ac neque nisi. Vivamus non orci sem, vel pharetra augue. Etiam arcu metus, elementum vitae lobortis ut, lacinia quis mauris. Pellentesque sodales, turpis scelerisque malesuada feugiat, nulla mi fermentum purus, et faucibus elit nisi vel leo. Integer nulla mauris, condimentum vel lobortis non, luctus convallis nisl. Vestibulum lacinia hendrerit mauris, at commodo dui convallis aliquam. Cras at orci ut urna imperdiet ornare.
+Aliquam non faucibus massa. Pellentesque pretium, arcu tristique varius eleifend, eros orci vulputate risus, id malesuada leo purus a est. Praesent ac arcu at erat volutpat adipiscing. Sed est lorem, ultricies quis tristique nec, ultricies a lectus. Aliquam ut porta diam. Aliquam nibh arcu, venenatis nec tempus sed, commodo quis enim. Ut elit mi, consequat non auctor vitae, commodo a augue. Quisque tincidunt porta urna rutrum malesuada. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur vitae felis eget lorem sodales ultricies elementum id odio. Aliquam sit amet massa quis tellus cursus venenatis. Pellentesque semper mi posuere erat ultrices porta. Aenean ac erat justo.
+Duis convallis iaculis eros, et sagittis felis accumsan non. In sit amet porta mauris. Sed quis lorem libero, a varius erat. Nulla facilisi. Duis malesuada eros et justo accumsan vulputate. In ultrices odio vel lacus dictum eu laoreet neque euismod. Proin ut nulla metus. Aliquam erat volutpat. In rutrum erat magna. Vivamus nec ultrices mauris. Sed quis neque pulvinar massa varius luctus. Aliquam ut magna lorem, vel varius magna. Maecenas commodo, lorem et porttitor ornare, eros augue ornare velit, a tristique purus elit sed nisl.
+Vusce nec mauris eu dolor hendrerit ullamcorper. Mauris leo felis, ornare sed rhoncus sed, viverra quis augue. Nullam ligula magna, fringilla at eleifend nec, vulputate sed sapien. Donec eget risus augue. Nunc ornare est vel orci sagittis nec luctus velit interdum. Vivamus dolor dolor, sagittis et tincidunt sit amet, tincidunt et purus. Aenean egestas, diam et lobortis consequat, nisi lectus accumsan quam, sit amet congue nibh urna vitae nisl. Sed a nulla est, convallis faucibus nisl. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ipsum tellus, consectetur non rhoncus eget, commodo non tortor. Maecenas vel elit vel erat vestibulum porttitor non venenatis mi. Praesent luctus lorem arcu. Proin sagittis, mauris vel fermentum fringilla, nisi magna auctor mi, in pellentesque dolor mi scelerisque urna.
+Sed id lectus ipsum. Suspendisse potenti. Donec ultrices sodales tincidunt. Donec non metus ut ante dictum luctus id non ante. Vestibulum dapibus lacinia aliquam. Vivamus laoreet lacus sit amet eros ultricies hendrerit. Aenean sagittis convallis nisi nec facilisis. Vivamus est dui, ultrices ut feugiat ac, auctor quis augue. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent venenatis lacus quis odio elementum porta bibendum tortor molestie. Donec commodo felis quis nisi volutpat volutpat venenatis dui mattis. Suspendisse suscipit consectetur felis eleifend fermentum.
+Vestibulum id neque leo, vel laoreet ante. Duis sit amet nibh vitae lectus porta pellentesque vel nec metus. Proin libero ligula, venenatis sit amet cursus at, vulputate in augue. Vivamus mi justo, auctor et ornare vel, volutpat vitae ligula. Vestibulum porttitor scelerisque libero, ut iaculis odio iaculis ut. Aenean porttitor facilisis magna id ullamcorper. Aenean vel orci libero. Curabitur dignissim suscipit purus. Aliquam in sem quis leo fermentum scelerisque id sed sem. Aliquam dapibus feugiat metus a blandit. Etiam semper faucibus est ornare consectetur. Phasellus blandit, nulla vitae laoreet blandit, dolor magna varius lectus, nec eleifend enim libero et lacus. Etiam varius interdum tortor, condimentum varius odio tempor vitae. In hac habitasse platea dictumst. "));
+
+$size = count($words);
+
+define('RESULTS', 100); //ilosc alertów na całej mapie
+define('RESULTS_SEARCH_MIN', 0); // minimalna ilość alertów w wynikach wyszukiwania
+define('RESULTS_SEARCH_MAX', 15); // maksymalna ilość alertów w wynikach wyszukiwania
+
+define('ALERTS', 'alerts');
+define('LAT_MIN', 50.327848);
+define('LAT_MAX', 53.732391);
+define('LNG_MIN', 15.011347);
+define('LNG_MAX', 22.504023);
+
+define('SCALE', 1000); // dokładność jaka nas interesuje
+
+define('LAT_MIN_SCALED', LAT_MIN * SCALE);
+define('LAT_MAX_SCALED', LAT_MAX * SCALE);
+define('LNG_MIN_SCALED', LNG_MIN * SCALE);
+define('LNG_MAX_SCALED', LNG_MAX * SCALE);
+
+if(isset($_GET['json'])){
+    
+    $input = $_GET['json'];
+    
+    $input = json_decode($input, true);
+    
+}
+$output = array(ALERTS => array());
+if ($input['ne'] != null && $input['sw'] != null) {
+    $results = mt_rand(RESULTS_SEARCH_MIN, RESULTS_SEARCH_MAX);
+    
+    
+    $lat_min_scaled = $input['sw']['lat'] * SCALE;
+    $lat_max_scaled = $input['ne']['lat'] * SCALE;
+    $lng_min_scaled = $input['sw']['lng'] * SCALE;
+    $lng_max_scaled = $input['ne']['lng'] * SCALE;
+    
+    for ($i = 0; $i < $results; $i++) {
+        $output[ALERTS][($i) * 10] = array(
+        	$i,
+        	mt_rand($lat_min_scaled, $lat_max_scaled) / SCALE, 
+        	mt_rand($lng_min_scaled, $lng_max_scaled) / SCALE, 
+        	mt_rand(1, 3), 
+        	mt_rand(1, 30), 
+        	mt_rand(1, 20), 
+        	$i.'-ta Przykładowa nazwa',
+        	$i.'-te Lorem ipsum dolor sit amet...',
+        	$i.'-ta lokacja',
+        	$i.'-ty użykownik'
+        );
+    }
+} else {
+    
+    for ($i = 0; $i < RESULTS; $i++) {
+        $output[ALERTS][($i) * 10] = array(
+        	$i,
+        	mt_rand(LAT_MIN_SCALED, LAT_MAX_SCALED) / SCALE, 
+        	mt_rand(LNG_MIN_SCALED, LNG_MAX_SCALED) / SCALE, 
+        	mt_rand(1, 3), 
+        	mt_rand(1, 30), 
+        	mt_rand(1, 20), 
+        	$i.'-ta Przykładowa nazwa',
+        	$i.'-te Lorem ipsum dolor sit amet...',
+        	$i.'-ta lokacja',
+        	$i.'-ty użykownik'
+        );
+    }
+}
+
+header('Content-Type: application/json');
+echo json_encode($output);
+
+function generateDescription($words, $maxLength = 140) {
+    $output = '';
+    $outputWordsCount = mt_rand(2, 40);
+    $wordsCount = count($words);
+    for ($i = 0; $i < $outputWordsCount; $i++) {
+        $output .= $words[mt_rand(0, $wordsCount)] . " ";
+    }
+
+    $output = substr($output, 0, 140);
+    $output = ucfirst($output);
+    return $output;
+}
+
+?>
