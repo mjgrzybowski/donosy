d263f2099990d760f756c62ef8e27c19834bba67
Paweł Mikołajczuk <pawel.mikolajczuk@strazpozarna.org.pl>
2011-07-04 12:34:32 +0200

    initial commit

---
+++ config.js
@@ -0,0 +1,71 @@
+var options = {
+  "config": {
+    "latlng" : "52.12324,21.242155",
+    "address" : "Polska, Warszawa, św. Franciszka Salezego 6",
+    "categories" : {
+      "icons" : [
+      {
+        1 : "icon1.png"
+      },
+      {
+        2 : "icon2.png"
+      },
+      {
+        3 : "car-accident.png"
+      },
+        
+      ],
+      "show" : [1,4,5,6,8,9,10,14],
+      "showAll" : false
+    }
+    
+  },
+  
+  "alerts" : {},
+  "polygons" : [
+    {
+      "paths" : [
+      [52.123425,21.2425],
+      [52.123425,21.2425],
+      [52.123425,21.2425],
+      [52.123425,21.2425],
+      
+      ],
+      "strokeColor" : "#FF0000",
+      "strokeOpacity" : 0.8,
+      "strokeWeight" : 2,
+      "fillColor" : "#FF0000",
+      "fillOpacity" : 0.35,
+      "zIndex" : 0
+    }
+  ],
+  "buttons" : [
+    {
+      "label" : "Nazwa przycisku",
+      "controlPosition" : google.maps.ControlPosition.BOTTOM_CENTER,
+      "click" : function(button){}
+    },
+    {
+      "label" : "Nazwa przycisku",
+      "controlPosition" : google.maps.ControlPosition.BOTTOM_CENTER,
+      "click" : function(button){}
+    },
+    
+  ],
+  
+  
+  "alert_click" : function(alert){},
+  "alert_mouserout" : function(alert){},
+  "alert_mouserover" : function(alert){},
+  "alert_dragend" : function(alert){},
+  
+  
+  "polygon_click" : function(polygon){},
+  "polygon_dblclick" : function(polygon){},
+  "polygon_mousedown" : function(polygon){},
+  "polygon_mousemove" : function(polygon){},
+  "polygon_mouseout" : function(polygon){},
+  "polygon_mouseover" : function(polygon){},
+  "polygon_mouseup" : function(polygon){},
+  "polygon_rightclick" : function(polygon){}
+}
---
+++ index.php
@@ -0,0 +1,95 @@
+
+<!DOCTYPE html>
+<html style="width: 100%; height: 100%;">
+  <head>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <script src="http://code.jquery.com/jquery-1.6.2.js" type="text/javascript"></script> 
+    <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script> 
+    <script src="http://google-maps-utility-library-v3.googlecode.com/svn/tags/markermanager/1.0/src/markermanager.js" type="text/javascript"></script> 
+    <script src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/src/markerclusterer.js" type="text/javascript"></script> 
+
+
+  </head>
+  <body  style="width: 100%; height: 100%; margin: 0;">
+    <div id="map" style="width: 100%; height: 100%;"></div> 
+
+    <script type="text/javascript"> 
+      var myOptions = {
+        zoom: 4,
+        center: new google.maps.LatLng(51.25, 21.00),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      var map = new google.maps.Map(document.getElementById('map'), myOptions);
+
+      
+      
+      var mcOptions = {gridSize: 30, maxZoom: 15};
+      var mc = new MarkerClusterer(map, [], mcOptions);
+      var bounds = new google.maps.LatLngBounds();
+        
+        $.ajax({
+          url: 'alerts.php',
+          success: function(data){
+            var batch = [];
+            
+            $.map(data.alerts, function(value){
+              
+              batch.push(new google.maps.Marker({
+                position: new google.maps.LatLng(value[0], value[1]),
+                title: 'Weather marker',
+                flat: true
+              }));
+              bounds.extend(new google.maps.LatLng(value[0], value[1]));
+              
+            });
+          
+           
+            mc.addMarkers(batch, 3);  
+            map.fitBounds(bounds);
+            
+          }
+        });
+                  
+       
+      
+     
+      
+      
+      var markers = {};
+      
+      
+      google.maps.event.addListener(map, 'idle', function(){
+        
+        
+      });
+
+      
+      function getRandomPoint() {
+        var lat = 48.25 + (Math.random() - 0.5) * 14.5;
+        var lng = 11.00 + (Math.random() - 0.5) * 36.0;
+        return new google.maps.LatLng(Math.round(lat * 10) / 10, Math.round(lng * 10) / 10);
+      }
+
+      function getWeatherMarkers(n) {
+        var batch = [];
+        for (var i = 0; i < n; ++i) {
+        
+          batch.push(new google.maps.Marker({
+            position: getRandomPoint(),
+            
+            title: 'Weather marker'
+          })
+        );        
+        }
+        return batch;
+      }
+       
+      
+
+      
+      
+      
+    </script> 
+  </body>
+</html>
