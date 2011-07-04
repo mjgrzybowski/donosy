3db09d56759320d94881251e197932d954dac77f
Konrad-Podgorski <konrad.podgorski@gmail.com>
2011-07-04 17:20:20 +0200

    .

--- mapView.js
+++ mapView.js
@@ -21,6 +21,22 @@
                     "maxZoom": 15
                 }
 
+            },
+            "event" :{
+                "alert_click" : null,
+                "alert_mouserout" : null,
+                "alert_mouserover" : null,
+                "alert_dragend" :  null,
+  
+  
+                "polygon_click" :  null,
+                "polygon_dblclick" :  null,
+                "polygon_mousedown" :  null,
+                "polygon_mousemove" :  null,
+                "polygon_mouseout" :  null,
+                "polygon_mouseover" :  null,
+                "polygon_mouseup" :  null,
+                "polygon_rightclick" : null
             }
     
         };
