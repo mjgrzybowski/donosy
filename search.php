c0724e662504b92ee001f60404eca2a72b03e339
Paweł Mikołajczuk <pawel.mikolajczuk@strazpozarna.org.pl>
2011-07-04 13:43:35 +0200

    add global object in search js

--- search.php
+++ search.php
@@ -7,10 +7,11 @@
     <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script> 
     <script src="http://google-maps-utility-library-v3.googlecode.com/svn/tags/markermanager/1.0/src/markermanager.js" type="text/javascript"></script> 
     <script src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/src/markerclusterer.js" type="text/javascript"></script> 
-
+	<script src="search.js"></script>
 
   </head>
   <body  style="width: 100%; height: 100%; margin: 0;">
-    <div id="map" style="width: 100%; height: 100%;"></div>
+    <div id="map" style="width: 100%; height: 100%;"></div> 
+
   </body>
 </html>
