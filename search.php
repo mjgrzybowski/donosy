71e815c06863b750e9938a19927d54fdaaed184c
Konrad-Podgorski <konrad.podgorski@gmail.com>
2011-07-04 20:55:55 +0200

    added images and css, prototype of buttons

--- search.php
+++ search.php
@@ -8,6 +8,7 @@
 	    <script src="http://google-maps-utility-library-v3.googlecode.com/svn/tags/markermanager/1.0/src/markermanager.js" type="text/javascript"></script> 
 	    <script src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/src/markerclusterer.js" type="text/javascript"></script>
 	    <script src="mapView.js" type="text/javascript"></script> 
+            <link href="css/main.css" media="screen" rel="stylesheet" type="text/css" />
 		<script src="search.js"></script>
   	</head>
 	<body style="width: 100%; height: 100%; margin: 0;">
